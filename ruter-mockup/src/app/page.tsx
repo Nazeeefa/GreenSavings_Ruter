fc38078f339a2dcb34afa836762e215617843553
Qupergo <sebastian.b.danckwardt@gmail.com>
2024-01-24 15:53:59 +0100

    Simple use effect api call start

--- ruter-mockup/src/app/page.tsx
+++ ruter-mockup/src/app/page.tsx
@@ -1,6 +1,26 @@
-import Image from "next/image";
+"use client";
+
+import { useState, useEffect } from "react";
 import { ExampleTrip } from "@/components/component/exampletrip";
 
+interface Station {
+  name: string;
+  travel_mode: string;
+}
+
 export default function Home() {
+  const [trip, setTrip] = useState<Station[]>([]);
+
+  useEffect(() => {
+    const getTrip = async () => {
+      const response = await fetch("/api/get-trip");
+      const data = await response.json();
+      console.log(data);
+      setTrip(data);
+    };
+    getTrip();
+    console.log(trip);
+  }, []);
+
   return ExampleTrip();
 }
