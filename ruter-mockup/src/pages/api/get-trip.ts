2cc43df82105066115d77def971a12e24311dd0a
VectorHector <erik.doering@t-online.de>
2024-01-24 19:10:02 +0100

    add car vs transit scores to api response

--- ruter-mockup/src/pages/api/get-trip.ts
+++ ruter-mockup/src/pages/api/get-trip.ts
@@ -1,16 +1,21 @@
-export default async function handler(req, res) {
-    if (req.method === 'POST') {
-        const { body } = req;
+const PT_30DAY_CARD_COST = 850
+const PT_SINGLE_TICKET_COST = 36
 
-        const { from, to } = body;
+const AVG_G_CO2_PER_KM_CAR = 64
+const AVG_G_CO2_PER_KM_PT = 22
 
-        const fromLongitude = from.longitude;
-        const fromLatitude = from.latitude;
-        const toLongitude = to.longitude;
-        const toLatitude = to.latitude;
+const AVG_COST_PER_KM_CAR = 10.24
+const AVG_COST_PER_KM_PT = 0.8
 
-        const query = `
-        query($fromLatitude: Float!, $fromLongitude: Float!, $toLatitude: Float!, $toLongitude: Float!, $dateTime: DateTime!)
+const AVG_NO_OF_TRIPS_PER_DAY = 3.29
+const AVG_DISTANCE_TRAVELED_PER_DAY = 33.6
+const AVG_DISTANCE_PER_TRIP = 10.2
+
+const TIME_SPENT_PARKING_S = 390  // 6.5 Minutes
+
+const route = async (fromLongitude, fromLatitude, toLongitude, toLatitude, mode: "car" | "transit") => {
+    const query = `
+        query($fromLatitude: Float!, $fromLongitude: Float!, $toLatitude: Float!, $toLongitude: Float!, $dateTime: DateTime!, $directMode: StreetMode!)
         {
   trip(
     from: {
@@ -28,60 +33,108 @@ export default async function handler(req, res) {
     numTripPatterns: 3
     dateTime: $dateTime
     walkSpeed: 1.3
-    modes: {directMode: car_pickup, transportModes: []}
+    modes:{accessMode:foot, egressMode:foot, directMode:$directMode}
     arriveBy: false
   )
   {
-    tripPatterns {
-      expectedStartTime
-      duration
-      walkDistance
-      legs {
-        mode
-        distance
-        line {
-          id
-          publicCode
-        }
-      }
-    }
-  }
+            tripPatterns {
+              duration
+              walkDistance
+              legs {
+                expectedStartTime
+                expectedEndTime
+                mode
+                distance
+                line {
+                  id
+                  publicCode
+                }
+              }
+            }
+          }
 }
 `
-        const dateTime = new Date().toISOString();
+    const dateTime = new Date().toISOString();
 
-        const variables = {
-            fromLatitude,
-            fromLongitude,
-            toLatitude,
-            toLongitude,
-            dateTime,
+    const variables = {
+        fromLatitude,
+        fromLongitude,
+        toLatitude,
+        toLongitude,
+        dateTime,
+        directMode: mode === "car" ? "car_pickup" : "flexible",
+    }
+
+    const url = 'https://api.entur.io/journey-planner/v3/graphql';
+
+    const reqBody = JSON.stringify({
+        query,
+        variables,
+    })
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'ET-Client-Name': 'ruter-mockup',
+            'ET-Client-ID': 'ruter-mockup',
+        },
+        body: reqBody,
+    };
+
+    const response = await fetch(url, options);
+
+    return await response.json();
+}
+
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const {body} = req;
+        const {from, to, hasMonthlyCard} = body;
+
+        const carData = await route(from.longitude, from.latitude, to.longitude, to.latitude, 'car');
+        const transitData = await route(from.longitude, from.latitude, to.longitude, to.latitude, 'transit');
+
+        const relevantTransitData = transitData.data.trip.tripPatterns;
+
+        const carDistanceKm = carData.data.trip.tripPatterns[0].legs.filter((leg) => leg.mode === 'car').reduce((acc, leg) => acc + leg.distance, 0) / 1000;
+        const carTravelTime = carData.data.trip.tripPatterns[0].duration;
+
+        const carCost = carDistanceKm * AVG_COST_PER_KM_CAR;
+        const carCo2g = carDistanceKm * AVG_G_CO2_PER_KM_CAR;
+
+        let PT_cost = PT_SINGLE_TICKET_COST;
+        if (hasMonthlyCard) {
+            const tripsPerMonth = AVG_NO_OF_TRIPS_PER_DAY * (365.24/12)
+            PT_cost = PT_30DAY_CARD_COST / tripsPerMonth;
         }
 
-        const url = 'https://api.entur.io/journey-planner/v3/graphql';
+        const out = relevantTransitData.map((tripPattern) => {
+            const ptDistanceKm = tripPattern.legs.filter((leg) => leg.mode !== 'car' && leg.mode !== "foot").reduce((acc, leg) => acc + leg.distance, 0) / 1000;
+            const ptTravelTime = tripPattern.duration;
 
-        const reqBody = JSON.stringify({
-            query,
-            variables,
+            const ptCo2g = ptDistanceKm * AVG_G_CO2_PER_KM_PT;
+
+            const durationEqualToCar = ptTravelTime <= carTravelTime + TIME_SPENT_PARKING_S;
+            const moneySaved = Math.round(carCost - PT_cost);
+            const co2Saved = Math.round(carCo2g - ptCo2g);
+            const durationSaved = Math.round(carTravelTime - ptTravelTime);
+            const distanceSaved = Math.round(carDistanceKm - ptDistanceKm);
+
+            return {
+                durationEqualToCar,
+                moneySaved,
+                co2Saved,
+                durationSaved,
+                distanceSaved,
+                ptTravelTime,
+                ptDistanceKm,
+                trip: tripPattern,
+            }
         })
 
-        console.log(reqBody);
-
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'ET-Client-Name': 'ruter-mockup',
-                'ET-Client-ID': 'ruter-mockup',
-            },
-            body: reqBody,
-        };
-
-        const response = await fetch(url, options);
-
-        const data = await response.json();
-
-        res.status(200).json(data);
+        res.status(200).json(out);
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
